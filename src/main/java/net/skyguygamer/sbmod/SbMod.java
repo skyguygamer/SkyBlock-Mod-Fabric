1b5723646b51cb2c62181e6a1b66a11e904886f8
skyguygamer <tristanburchard@gmail.com>
2022-10-30 14:14:15 -0400

    Initial commit

---
+++ src/main/java/net/skyguygamer/sbmod/SbMod.java
@@ -0,0 +1,16 @@
+package net.skyguygamer.sbmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SbMod implements ModInitializer {
+
+	public static final String SBMOD_ID = "sbmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(SBMOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/skyguygamer/sbmod/SbModClient.java
@@ -0,0 +1,10 @@
+package net.skyguygamer.sbmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SbModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
